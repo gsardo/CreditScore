855f86b5da7b51a572966f6c42d77b1f7ee8eaa9
Giuseppe <giuseppe.sardo@clearscore.com>
2023-07-03 12:51:48 +1000

    Initial Commit

---
+++ CreditScore/CreditScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditScoreApp.swift
+//  CreditScore
+//
+//  Created by Giuseppe Sardo on 3/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct CreditScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
